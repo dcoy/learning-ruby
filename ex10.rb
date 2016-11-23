720061b1b3355e246d815082fce08311dbff383a
David Coy <david@obsidianspork.io>
2016-11-23 15:34:08 -0600

    Add my own examples

---
+++ ex10.rb
@@ -0,0 +1,30 @@
+# Exercise 10: What was that?
+
+
+# Exercise example
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+# Using ''' instead of """ will not register it as a
+# multi-line string.
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+# Own examples
+
+puts """
+\nHello there!\n
+Here's a string of text!
+\vHere's the third line\b
+And a \rfourth!
+Finally, a \a5th."""
