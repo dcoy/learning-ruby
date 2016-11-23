1a8d63b427306f04485b7928ac5a12563af4ce4b
David Coy <david@obsidianspork.io>
2016-11-23 14:47:29 -0600

    Using newline and multi-line strings

---
+++ ex9.rb
@@ -0,0 +1,15 @@
+# Exercise 9: Printing, Printing, Printing
+days = "Mon Tues Wed Thu Fri Sat Sun"
+# Hit em with a new line after each month
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+# Multi-line string
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
