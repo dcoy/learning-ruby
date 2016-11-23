77af6294097f56cc628567c31fddf0fb9699c30f
David Coy <david@obsidianspork.io>
2016-11-23 13:21:44 -0600

    Explain what happens

--- ex3.rb
+++ ex3.rb
@@ -1,15 +1,19 @@
 puts "I will now count my chickens:"
 
+# This will calculate the amount of hens and roosters
 puts "Hens #{25.0 + 30.0 / 6.0}"
 puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 puts "Now I will count the eggs:"
 
+# This will calculate the amount of eggs
 puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
-puts "Is it true that 3 + 2 < 5 - 7?"
 
+puts "Is it true that 3 + 2 < 5 - 7?"
+# this will return false
 puts 3.0 + 2.0 < 5.0 - 7.0
 
+# This will calculate the given floating point numbers
 puts "What is 3 + 2? #{3.0 + 2.0}"
 puts "what is 5 - 7? #{5.0 - 7.0}"
 
@@ -17,6 +21,7 @@ puts "Oh, that's why it's false."
 
 puts "How about some more."
 
+# Using comparison operators -- outputs True or False
 puts "Is it greater? #{5.0 > -2.0}"
 puts "Is it greater or equal? #{5.0 >= -2.0}"
 puts "Is it less or equal? #{5.0 <= -2.0}"
