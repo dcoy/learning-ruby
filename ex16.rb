3346578dfa74e8eba3a24913e0ec9f8a32e14c75
David Coy <david@obsidianspork.io>
2016-12-01 20:31:42 -0600

    Adding a little text decoration in here

--- ex16.rb
+++ ex16.rb
@@ -1,6 +1,19 @@
-filename = ARGV.first
+# Will come back to this to wrap these into a
+# function or two.
 
-puts "We're going to erase #{filename}"
+filename = ARGV.first
+# Let's read the file first
+puts "First, we are going to output the original contents:"
+file = open(filename)
+print '########################'
+print "\n"
+print "#"
+print file.read
+print "\n"
+print '########################'
+puts "\n"
+# Now we can truncate the contents of the file
+puts "Now, we're going to erase #{filename}"
 puts "If you don't want that, hit CTRL-C (^C)."
 puts "If you do want that, hit RETURN."
 
@@ -14,11 +27,11 @@ target.truncate(0)
 
 puts "Now I'm going to ask you for three lines."
 
-print "line 1: "
+print "Line 1: "
 line1 = $stdin.gets.chomp
-print "line 2: "
+print "Line 2: "
 line2 = $stdin.gets.chomp
-print "line 3: "
+print "Line 3: "
 line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
