9d412b5baabe611ef0fa5bc5cfa760e2796a2c33
David Coy <david@obsidianspork.io>
2016-12-03 13:51:47 -0600

    Extra Credit: loop through array, output array val, index

--- ex34.rb
+++ ex34.rb
@@ -10,3 +10,14 @@ puts "5. The fifth(5th) animal is a #{animals[4]}."
 puts "6. The animal at 2 is a #{animals[2]}."
 puts "7. The sixth(6th) animal is a #{animals[5]}."
 puts "8. The animal at 4 is a #{animals[4]}."
+
+# Extra credit
+
+languages = ['ruby', 'python', 'golang', 'perl', 'haskel', 'javascript']
+
+
+# iterate through 'languages' array, print each lang and
+# index position
+languages.each_with_index do |lang, index|
+  puts "The language is #{lang}, at index position #{index}."
+end
