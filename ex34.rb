b760800920c8a32701997dc1a835b9ddacfee252
David Coy <david@obsidianspork.io>
2016-12-03 13:51:21 -0600

    Completed original exercise

---
+++ ex34.rb
@@ -0,0 +1,12 @@
+# Accessing Elements of Arrays
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "1. The animal at one is a #{animals[1]}."
+puts "2. The third(3rd) animal is a #{animals[2]}."
+puts "3. The first(1st) animal is a #{animals[0]}."
+puts "4. The animal at 3 is a #{animals[3]}."
+puts "5. The fifth(5th) animal is a #{animals[4]}."
+puts "6. The animal at 2 is a #{animals[2]}."
+puts "7. The sixth(6th) animal is a #{animals[5]}."
+puts "8. The animal at 4 is a #{animals[4]}."
