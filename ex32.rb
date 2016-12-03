999f929d242e46bb08571bd21410c93459c8f30f
David Coy <david@obsidianspork.io>
2016-12-02 21:40:40 -0600

    Making changes to be more "Ruby"

--- ex32.rb
+++ ex32.rb
@@ -6,7 +6,8 @@ change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
 # first for-loop iterates through the list
 # similar to other languages
-for number in the_count
+# similar to 'for number in the_count'
+the_count.each do |number|
   puts "This is count #{number}."
 end
 
@@ -26,10 +27,12 @@ change.each { |i| puts "I got #{i}" }
 elements = []
 
 # then use the range operator to do 0 to 5 counts
+# (0...5) will not include the '5'
 (0..5).each do |i|
   puts "adding #{i} to the list."
   # push the i variable on the *end* of the list
-  elements.push(i)
+  # similar to elements.push(i)
+  elements << i
 end
 
 # now we can print them out too
