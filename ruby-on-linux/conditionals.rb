7bf34db990a2f39331ddbb0ce3998bad3d0f747a
David Coy <david@obsidianspork.io>
2016-12-21 20:45:14 -0600

    Conditionals: if, unless, etc.

--- ruby-on-linux/conditionals.rb
+++ ruby-on-linux/conditionals.rb
@@ -2,4 +2,15 @@ my_variable = 0
 
 if my_variable > 10
     puts my_variable
+elsif
+    puts "Your variable #{my_variable} is less than 10."
+else
+    puts "You're crazy!"
 end
+
+
+# One-line conditionals
+my_variable = 5
+
+puts "Big Number" if my_variable > 10
+puts "Small Number" unless my_variable < 10
