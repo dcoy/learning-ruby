ac8bbf109b680ba8305fecd932b3f16a89b6b19f
David Coy <david@obsidianspork.io>
2016-12-21 20:39:35 -0600

    Using math operators

---
+++ ruby-on-linux/basic_math.rb
@@ -0,0 +1,23 @@
+# Addition +
+addition = 3 + 5
+
+# Subtraction -
+subtraction = 5 - 3
+
+# Multiplication *
+multiplication = 3 * 5
+
+# Division /
+division = 15 / 3
+
+# Exponent **
+exponent = 5 ** 3
+
+# Modulus %
+modulus = 5 % 3
+
+# Math::PI
+math_pi = Math::PI
+
+# Print them all
+puts addition.to_s, subtraction.to_s, multiplication.to_f, division.to_f, exponent, modulus, math_pi
