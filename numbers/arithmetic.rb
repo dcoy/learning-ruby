ed7689eb6cc77fad849a80d4d282c9ae62769ef7
David Coy <david@obsidianspork.io>
2017-04-24 18:13:40 -0500

    Working with numbers

---
+++ numbers/arithmetic.rb
@@ -0,0 +1,17 @@
+# Addition
+puts 5 + 5
+
+# Subtraction
+puts 5 - 5
+
+# Multiplication
+puts 5 * 5
+
+# Division
+puts 5 / 5
+
+# Exponent
+puts 5 ** 5
+
+# Order of operations - PEMDAS
+puts 5 + 15 * 20 - 2 / 6 ** 3 - (3 + 1)
---
+++ numbers/integer_floats.rb
@@ -0,0 +1,5 @@
+# Difference between integers, floats and decimals
+
+puts "This is a float - 20.0 + 15 = #{20.0 + 15}."
+puts "This is an integer - 20 + 15 = #{20 + 15}."
+puts "This is a decimal - 20.3256981425 + 15 = #{20.3256981425 + 15}."
