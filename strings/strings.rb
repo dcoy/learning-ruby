b7d37eebe81800da7e46be5e69c2e56397fffef1
David Coy <david@obsidianspork.io>
2017-04-24 18:13:32 -0500

    Working with strings

---
+++ strings/strings.rb
@@ -0,0 +1,12 @@
+# Manipulating strings
+
+str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed sem quis sapien mattis maximus."
+
+# Lowercase
+puts str.downcase
+
+# Upper case
+puts str.upcase
+
+# Split
+puts str.split
